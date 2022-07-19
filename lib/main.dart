3068e8345cfca5b80994d4eb413e98bc329c62bd
dmprabath <dmprabath1995@gmail.com>
2022-07-19 22:50:55 +0530

    before add floating modal

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './transaction.dart';
+import './widgets/user_transaction.dart';
+import './widgets/new_transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,82 +15,56 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transaction = [
-    Transaction(id: '1', title: "first", amount: 200.00, date: DateTime.now()),
-    Transaction(id: '2', title: "second", amount: 300.00, date: DateTime.now()),
-  ];
+  // late String titleInput;
+  // late String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  // void startAddNewTransaction(BuildContext ctx) {
+  //   showModalBottomSheet(
+  //     context: ctx,
+  //     builder: (_) {
+  //       return NewTransaction(addTx);
+  //     },
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expenses Manager'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Card(
-            color: Colors.blue,
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              width: double.infinity,
-              child: const Text(
-                "Expenses",
-                style: TextStyle(color: Colors.white),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            elevation: 5,
-          ),
-          // Card(
-          //   color: Colors.red,
-          //   child: Text("Second"),
-          // )
-          Column(
-            children: transaction.map((tx) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      )),
-                      child: Text(
-                        tx.amount.toString(),
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: const TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          tx.date.toString(),
-                          style:
-                              const TextStyle(fontSize: 16, color: Colors.grey),
-                        ),
-                      ],
-                    )
-                  ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              color: Colors.blue,
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                width: double.infinity,
+                child: const Text(
+                  "Expenses",
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
                 ),
-              );
-            }).toList(),
-          )
-        ],
+              ),
+              elevation: 5,
+            ),
+            UserTransaction()
+            // Card(
+            //   color: Colors.red,
+            //   child: Text("Second"),
+            // )
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => {},
       ),
     );
   }
